7aaffa8a8aa75b1188a208184c6e5ac9f2f7b28f
abiodun603 <abioduncsc@gmail.com>
2024-07-06 22:50:05 +0100

    product set up and schema definition

---
+++ src/controllers/authentication.ts
@@ -0,0 +1,84 @@
+import express from 'express';
+
+import { createUser, getUserByEmail } from '../db/models/users';
+import { authentication, random } from '../helpers';
+
+export const login = async ( req: express.Request, res: express.Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if(!email || !password) {
+      return res.sendStatus(400)
+    }
+
+    const user = await getUserByEmail(email).select('+authentication.salt +authentication.password');
+
+    if(!user) {
+      return res.sendStatus(400)
+    }
+
+    // authenticate user without knowing there password using the hash comparison 
+    const expectedHash = authentication(user?.authentication?.salt, password)
+
+    if(user.authentication.password !== expectedHash) {
+      return res.sendStatus(403);
+    }
+
+    const salt = random();
+    user.authentication.sessionToken = authentication(salt, user._id.toString());
+
+    await user.save();
+
+    res.cookie('ABIODUN-AUTH', user.authentication.sessionToken, {
+      domain: 'localhost',
+      path: "/"
+    })
+
+    return res.status(200).json(user).send()
+
+  } catch (error) {
+    
+  }
+}
+
+
+export const register = async ( req: express.Request, res: express.Response ) => {
+
+  try {
+    const { email, password, username } = req.body;
+
+    console.log(email, password, username);
+
+    if ( !email || !password  || !username ) {
+      return res.sendStatus(400);
+    }
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+      console.log(existingUser, "hmmm");
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    const salt = random();
+
+    const user = await createUser({
+      username,
+      email,
+      authentication: {
+        salt,
+        password: authentication(salt, password),
+      }
+    })
+
+    console.log(user)
+
+
+
+    return res.status(200).json(user).end();
+  } catch ( err ) {
+    console.log( err );
+    return res.sendStatus( 400 );
+  }
+
+}
---
+++ src/db/models/category.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { IUser } from "./users";
+
+// Define the interface for the blog model
+export interface ICategory extends Document {
+  name: string;
+  color: string;
+  user: IUser["_id"]
+}
+
+// Define the schema for the category model
+const categorySchema = new mongoose.Schema<ICategory>({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  color: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  }
+})
+
+const Category = mongoose.model<ICategory>('Category', categorySchema);
+
+export default Category
