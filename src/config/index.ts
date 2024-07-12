e7a101535bff7191da0b622bbf187ae379c215f7
abiodun603 <abioduncsc@gmail.com>
2024-07-12 23:38:23 +0100

    env: change APP_PORT to PORT

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const config= {
   app_name: process.env['APP_NAME'] ?? 'Blog API',
-  app_port: process.env.APP_PORT || 4000,
+  app_port: process.env.PORT || 4000,
   db_url: process.env['DB_URL'] ?? 'mongodb+srv://abiodun_mastery:Testing123@cluster0.jupgc1f.mongodb.net/affiliate',
 
   aws: {
