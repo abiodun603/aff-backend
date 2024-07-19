94c626b932147c4f4c225bfa6d3ddf5a6ef73b60
abiodun603 <abioduncsc@gmail.com>
2024-07-19 22:51:37 +0100

    fix: remove tag to be required from blog schema

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -5,22 +5,26 @@ import { get } from 'lodash';
 async function createBlogPost ( req: express.Request, res: express.Response) {
   try {
 
-    const { title, slug, category, tags, content, images, status} = req.body
+    const { title, slug, category, content, images, status} = req.body
 
     // Safely retrieve the user's ID using lodash's get function
     const userId = get(req, 'identity._id');
 
-    if(!title || !slug || !category || !tags || !content || !images) {
+    if(!title || !slug || !category  || !content || !images) {
       return res.status(400).json({ message: "All fields are required." });
     }
 
     const blogPost = new BlogPostModel({
-      title, slug, category, tags, content, images, userId, status
+      title, slug, category, content, images, userId, status
     });
 
     blogPost.save()
 
-    return res.status(200).json(blogPost)
+    return res.status(200).json({
+      status: "SUCCESS",
+      message: "Request completed successfully",
+      blogPost
+    })
     
   } catch (error) {
 
